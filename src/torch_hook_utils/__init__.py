067f24c6266536fc03d3a895b7f3af590be59a6b
Fabrice Normandin <fabrice.normandin@gmail.com>
2023-10-26 14:35:52 -0400

    Update __init__.py

--- src/torch_hook_utils/__init__.py
+++ src/torch_hook_utils/__init__.py
@@ -1,4 +1,4 @@
-from .input_output_hooks import get_layer_inputs, get_layer_outputs, replace_layer_inputs
+from .input_output_hooks import get_layer_inputs, get_layer_outputs, replace_layer_inputs, replace_layer_outputs
 from .maxpool_hooks import save_maxpool_indices, use_indices_in_maxunpool
 from .layers import Sample, Sequential, Module, Lambda
 from .types import named_modules_of_type
@@ -7,6 +7,7 @@ __all__ = [
     "get_layer_inputs",
     "get_layer_outputs",
     "replace_layer_inputs",
+    "replace_layer_outputs",
     "save_maxpool_indices",
     "use_indices_in_maxunpool",
     "Sample",
