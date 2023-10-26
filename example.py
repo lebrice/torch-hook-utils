0a8e54d5ecac3c1eb1ad2e50e1a473208c36178a
Fabrice Normandin <normandf@mila.quebec>
2023-10-26 12:33:26 -0400

    Fix example, remove debugging call
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- example.py
+++ example.py
@@ -2,9 +2,9 @@ import torch
 import torch.distributions as dist
 from torch import nn
 
-from hook_utils import get_layer_inputs
-from hook_utils.layers import Lambda, Sample, Sequential
-from hook_utils.types import named_modules_of_type
+from torch_hook_utils import get_layer_inputs
+from torch_hook_utils.layers import Lambda, Sample, Sequential
+from torch_hook_utils.types import named_modules_of_type
 
 model = Sequential(
     nn.Flatten(),
