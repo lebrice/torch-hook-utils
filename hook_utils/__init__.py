751f2ddfceed30a65ac306b2175c2bcb3e3e581a
Fabrice Normandin <normandf@mila.quebec>
2023-10-26 10:22:50 -0400

    Rename `set_layer_inputs`
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- hook_utils/__init__.py
+++ hook_utils/__init__.py
@@ -1,4 +1,4 @@
-from .input_output_hooks import get_layer_inputs, get_layer_outputs, set_layer_inputs
+from .input_output_hooks import get_layer_inputs, get_layer_outputs, replace_layer_inputs
 from .maxpool_hooks import save_maxpool_indices, use_indices_in_maxunpool
 from .layers import Sample, Sequential, Module, Lambda
 from .types import named_modules_of_type
@@ -6,7 +6,7 @@ from .types import named_modules_of_type
 __all__ = [
     "get_layer_inputs",
     "get_layer_outputs",
-    "set_layer_inputs",
+    "replace_layer_inputs",
     "save_maxpool_indices",
     "use_indices_in_maxunpool",
     "Sample",
--- hook_utils/input_output_hooks.py
+++ hook_utils/input_output_hooks.py
@@ -7,6 +7,7 @@ from typing import Any, Generator, Iterable
 from torch import Tensor, nn
 from torch.utils.hooks import RemovableHandle
 from typing_extensions import Concatenate
+
 from .types import Module, OutT, T
 
 
@@ -58,55 +59,6 @@ def get_layer_outputs(
         hook_handle.remove()
 
 
-@contextlib.contextmanager
-def save_all_module_inputs_and_outputs(
-    layers: Module[Concatenate[T, ...], OutT]
-    | Iterable[tuple[str, Module[Concatenate[T, ...], OutT]]]
-) -> Generator[tuple[dict[str, T], dict[str, OutT]], None, None]:
-    named_layers = layers.named_modules() if isinstance(layers, nn.Module) else layers
-    layer_inputs: dict[str, T] = {}
-    layer_outputs: dict[str, OutT] = {}
-    hook_handles: list[RemovableHandle] = []
-
-    for name, layer in named_layers:
-        assert isinstance(layer, nn.Module)
-        layer_hook = partial(
-            _save_input_and_output_hook,
-            name,
-            layer_inputs,
-            layer_outputs,
-        )
-        hook_handle = layer.register_forward_hook(layer_hook, with_kwargs=True)
-        hook_handles.append(hook_handle)
-
-    yield layer_inputs, layer_outputs
-
-    for hook_handle in hook_handles:
-        hook_handle.remove()
-
-
-def _save_input_and_output_hook(
-    _layer_name: str,
-    _inputs: dict[str, T],
-    _outputs: dict[str, OutT],
-    /,
-    module: Module[Concatenate[T, ...], OutT],
-    args: tuple[T, ...],
-    kwargs: dict[str, Any],
-    output: OutT,
-):
-    """Saves the layer inputs and outputs in the given dictionaries.
-
-    This should only really be used with a `functools.partial` that pre-binds the layer name,
-    as well as the input and output dictionaries.
-
-    NOTE: This only saves the first input to the network.
-    """
-    if len(args) >= 1:
-        _inputs[_layer_name] = args[0]
-    _outputs[_layer_name] = output
-
-
 def _save_input_hook(
     _layer_name: str,
     _inputs: dict[str, T],
@@ -145,25 +97,8 @@ def _save_output_hook(
     _outputs[_layer_name] = output
 
 
-def _replace_layer_input_hook(
-    _name: str,
-    _inputs_to_use: dict[str, Tensor],
-    module: nn.Module,
-    args: tuple[Tensor, ...],
-):
-    assert len(args) == 1
-    replaced_inputs = _inputs_to_use[_name]
-    if not isinstance(replaced_inputs, tuple):
-        replaced_inputs = (replaced_inputs,)
-    assert len(replaced_inputs) == len(args)
-    assert all(
-        replaced_input.shape == arg.shape for replaced_input, arg in zip(replaced_inputs, args)
-    )
-    return replaced_inputs
-
-
 @contextlib.contextmanager
-def set_layer_inputs(
+def replace_layer_inputs(
     network_layers: Iterable[tuple[str, nn.Module]],
     inputs_to_use: dict[str, Tensor],
 ):
@@ -179,3 +114,20 @@ def set_layer_inputs(
 
     for handle in handles:
         handle.remove()
+
+
+def _replace_layer_input_hook(
+    _name: str,
+    _inputs_to_use: dict[str, Tensor],
+    module: nn.Module,
+    args: tuple[Tensor, ...],
+):
+    assert len(args) == 1
+    replaced_inputs = _inputs_to_use[_name]
+    if not isinstance(replaced_inputs, tuple):
+        replaced_inputs = (replaced_inputs,)
+    assert len(replaced_inputs) == len(args)
+    assert all(
+        replaced_input.shape == arg.shape for replaced_input, arg in zip(replaced_inputs, args)
+    )
+    return replaced_inputs
